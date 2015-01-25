f77e8d6146eeeb1d0103d166d9a9824e82edfda0
codelogicws <russ@codelogic.ws>
2015-01-24 23:47:27 -0800

    Quick Union Weighted algo added.

--- src/ws/codelogic/algorithms/union/UnionFactory.java
+++ src/ws/codelogic/algorithms/union/UnionFactory.java
@@ -11,10 +11,24 @@ public class UnionFactory {
         return new QuickUnion(getOrderedArray(size));
     }
 
+    public static Union makeQuickUnionWeighted(int size){
+        Integer[] branchPointer = getOrderedArray(size);
+        Integer[] treeSize = getRepeatArray(size, 1);
+        return new QuickUnionWeighted(branchPointer, treeSize);
+    }
+
     private static Integer[] getOrderedArray(int size) {
         Integer[] array = new Integer[size];
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
+        }
+        return array;
+    }
+
+    private static Integer[] getRepeatArray(int size, int fillArrayWith){
+        Integer[] array = new Integer[size];
+        for(int i=0;i<size;i++){
+            array[i] = fillArrayWith;
         }
         return array;
     }
