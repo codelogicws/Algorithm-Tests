1419d1487bc747231ca812e0d38d02dbbd343b73
codelogicws <russ@codelogic.ws>
2015-02-02 14:28:51 -0800

    refactored InsertionSort

--- src/ws/codelogic/algorithms/sort/InsertionSort.java
+++ src/ws/codelogic/algorithms/sort/InsertionSort.java
@@ -3,6 +3,7 @@ package ws.codelogic.algorithms.sort;
 public class InsertionSort {
 
     private final Comparable[] array;
+    private int currentIndex;
 
     public InsertionSort(Comparable[] array){
         this.array = array;
@@ -10,16 +11,25 @@ public class InsertionSort {
 
     public void sort() {
         for(int i=0;i<array.length;i++){
-            int currentIndex = i;
-            for(int x=i-1;x>=0;x--){
-                if(array[currentIndex].compareTo(array[x]) < 0){
-                    Comparable temp = array[currentIndex];
-                    array[currentIndex] = array[x];
-                    array[x] = temp;
-                    currentIndex = x;
-                }
+            currentIndex = i;
+            sortIOnLeftSideOfArray(i);
+        }
+    }
+
+    private void sortIOnLeftSideOfArray(int i) {
+        for(int x=i-1;x>=0;x--){
+            boolean outOfOrder = array[currentIndex].compareTo(array[x]) < 0;
+            if(outOfOrder){
+                swapValues(x);
             }
         }
+    }
+
+    private void swapValues(int x) {
+        Comparable temp = array[currentIndex];
+        array[currentIndex] = array[x];
+        array[x] = temp;
+        currentIndex = x;
     }
 
     public Comparable[] getArray() {
