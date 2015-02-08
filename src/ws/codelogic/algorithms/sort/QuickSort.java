ccf39e4695e07763e4932ab96f2ae5a5f6c62d5a
codelogicws <russ@codelogic.ws>
2015-02-07 19:05:22 -0800

    QuickSort is working

--- src/ws/codelogic/algorithms/sort/QuickSort.java
+++ src/ws/codelogic/algorithms/sort/QuickSort.java
@@ -2,14 +2,70 @@ package ws.codelogic.algorithms.sort;
 
 public class QuickSort extends Sort{
 
+    private Comparable temp;
+
     public QuickSort(Comparable[] array){
         this.array = array;
     }
 
     @Override
     public void sort() {
-
+        recursiveSort(0, array.length-1);
     }
 
+    private void recursiveSort(int low, int high){
+        int mid = partitioning(low, high);
+        if(low < mid-1){
+            recursiveSort(low, mid-1);
+        }
+        if(mid+1 < high){
+            recursiveSort(mid + 1, high);
+        }
+        System.out.println("Test");
+        debugPrintArray();
+    }
+
+    public int partitioning(int low, int high){
+        int pivot = low;
+        high++;
+
+        while(low<=high) {
+            while (less(array[++low], array[pivot])) ;
+            while (less(array[pivot], array[--high])) ;
+            debug(low, high);
+            swap(low, high);
+            System.out.println("Swtich");
+            debug(low, high);
+        }
+        swap(pivot, high);
+        debugPrintArray();
+        return high;
+    }
+
+    private void debugPrintArray() {
+        for(Comparable c : array){
+            printWithComma(c);
+        }
+    }
+
+    private void debug(int low, int high){
+        System.out.println("Debug-QuickSort low is: " + array[low] + " high is: " + array[high]);
+    }
+
+    private void printWithComma(Comparable c){
+        System.out.print(c + ", ");
+    }
+
+    private void swap(int low, int high) {
+        if(low<high){
+            temp = array[low];
+            array[low] = array[high];
+            array[high] = temp;
+        }
+    }
+
+    private boolean less(Comparable less, Comparable more){
+        return less.compareTo(more) == -1;
+    }
 
 }
--- src/ws/codelogic/algorithms/sort/QuickSortTest.java
+++ src/ws/codelogic/algorithms/sort/QuickSortTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.*;
 public class QuickSortTest {
 
     private Sort quickSort;
-    private Integer[] unsorted = {9,8,7,4,3,2,6,5,1};
-    private Integer[] sorted = {1,2,3,4,5,6,7,8,9};
+    private Integer[] unsorted = {5,4,3,6,7,2,1,5,9};
+    private Integer[] sorted = {1,2,3,4,5,5,6,7,9};
 
     @Before
     public void setup() {
